1cd38f681b74b8aba63979a99b85744dbb314fcc
JoeM <joe.moloney496@gmail.com>
2020-10-07 11:28:50 +0100

    RSA_App Project Initial Commit

---
+++ src/Classes/ImagePanel.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Classes;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author K00243015
+ */
+public class ImagePanel extends JPanel
+{
+    Image image;
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+    public ImagePanel()
+    {
+        image = toolkit.getImage(this.getClass().getResource("/IMG/ls.gif"));
+    }
+    
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        if (image != null)
+            g.drawImage(image, 0, 0, this);
+    }
+}
